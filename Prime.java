2a39fd891e334db706ab768a6d277e9018927dda
Ashwini Deivasigamani <119685957+ashwinideivasigamani@users.noreply.github.com>
2022-12-13 20:10:19 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,28 @@
+package taskprogram;
+
+public class Prime {
+
+	public static void main(String[] args) {
+	
+		int i;
+		int j;
+		
+		for(i=1;i<=20;i++)
+	 
+		{
+			int count=0;
+			for(j=1;j<=i;j++)
+			{
+			if(i%j==0)
+			count++;
+			
+			}
+			
+			if(count==2)
+			{
+			System.out.println(i);
+		}
+
+	}
+	}
+}
