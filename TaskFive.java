c2b3cae13b7365e548677e536d439b090459f8a8
Ashwini Deivasigamani <119685957+ashwinideivasigamani@users.noreply.github.com>
2022-12-13 21:52:43 +0530

    Add files via upload

---
+++ TaskFive.java
@@ -0,0 +1,25 @@
+package taskprogram;
+
+		import java.util.*;
+		public class TaskFive {
+		 public static void main(String[] args) {
+		  String str1 = "best of luck to all!";
+		  System.out.println("The given string is: " + str1);
+		  System.out.println("After removing duplicates characters the new string is: " + removeDuplicateChars(str1));
+		 
+		  }
+		  private static String removeDuplicateChars(String sourceStr) {
+		   char[] arr1 = sourceStr.toCharArray();
+		   String targetStr = "";
+		   for (char value: arr1) {
+		    if (targetStr.indexOf(value) ==-1) {
+		     targetStr += value;
+		    }
+		   }
+		   return targetStr;
+		  }
+
+	
+
+		  
+}
